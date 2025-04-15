9d9f25bc707bee5ec895fde712726d45861febed
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-04-15 17:05:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/KomalMUSICUPDATE")
