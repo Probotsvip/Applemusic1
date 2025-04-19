0bffb592a3aa879c0bbfe669522c4935706cb19b
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-04-19 13:24:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,8 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BestFriendsChattingZone")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
+SONG_DOWNLOAD_DURATION=180
+
 # Chat id of a group for logging bot's activities
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", -1002023049910))
 
