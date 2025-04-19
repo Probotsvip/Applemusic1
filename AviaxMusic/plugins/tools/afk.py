e079f172e8fd9a0e20f263d30fa621b1ebf749bc
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-04-19 11:28:16 +0530

    Update afk.py

--- AviaxMusic/plugins/tools/afk.py
+++ AviaxMusic/plugins/tools/afk.py
@@ -3,9 +3,9 @@ from config import BOT_USERNAME
 from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
-from SHUKLAMUSIC import app
-from SHUKLAMUSIC.mongo.readable_time import get_readable_time
-from SHUKLAMUSIC.mongo.afkdb import add_afk, is_afk, remove_afk
+from AviaxMusic import app
+from AviaxMusic.mongo.readable_time import get_readable_time
+from AviaxMusic.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
 
