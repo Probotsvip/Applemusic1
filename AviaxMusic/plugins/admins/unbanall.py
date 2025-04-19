a7b259c24959b67353c541ac3dfc82a82479c49c
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-04-19 11:07:34 +0530

    Update unbanall.py

--- AviaxMusic/plugins/admins/unbanall.py
+++ AviaxMusic/plugins/admins/unbanall.py
@@ -1,8 +1,8 @@
-from SHUKLAMUSIC import app
+from AviaxMusic import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from SHUKLAMUSIC.utils.Shukla_ban import admin_filter
+from AviaxMusic.utils.Shukla_ban import admin_filter
 
 BOT_ID = "6824607634"
 
