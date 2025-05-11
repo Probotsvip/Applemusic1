6b77ca0924f5bc457067e0cfc6ccd1d0fb01469c
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-05-11 20:11:10 +0530

    Update Youtube.py

--- AviaxMusic/platforms/Youtube.py
+++ AviaxMusic/platforms/Youtube.py
@@ -28,8 +28,8 @@ async def shell_cmd(cmd):
 
 
 async def get_stream_url(query, video=False):
-    api_url = "http://0.0.0.0:1470"
-    api_key = "your_secret_key_here"
+    api_url = "http://46.250.243.87:1470/youtube"
+    api_key = "1a873582a7c83342f961cc0a177b2b26"
     
     async with httpx.AsyncClient(timeout=60) as client:
         params = {"query": query, "video": video, "api_key": api_key}
